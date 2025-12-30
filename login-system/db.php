ddf94a63d1396018bb35286f55537b7cefcae2cb
Fardinmustafi <fmustafi2330111@bscse.uiu.ac.bd>
2025-12-30 21:12:34 +0600

    Add files via upload

---
+++ login-system/db.php
@@ -0,0 +1,14 @@
+<?php
+// Database connection
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "login_system";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
